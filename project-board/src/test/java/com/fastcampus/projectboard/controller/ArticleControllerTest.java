422d79afcc7f0e36e6b4f1705784f0ef8786ad5f
jm1030 <yyyeeeooo01@naver.com>
2024-04-10 16:42:11 +0900

    게시판 뷰 기본 골격 작성

--- project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -22,19 +22,20 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
+    // test 시 No matching tests found in any candidate test task. 에러 발생하면
+    // File > Settings > Build,Execution,Deployment > Build Tools > Gradle > Runner > "Run tests using:"
+    // IntelliJ IDEA로 바꾸기
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
-    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws  Exception {
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         // Given
 
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
-
     }
 
     @Disabled("구현 중")
